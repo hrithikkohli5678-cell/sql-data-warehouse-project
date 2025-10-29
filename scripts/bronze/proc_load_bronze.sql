39cf976173f6b68dc72d7ac9ce76adb21592e0d0
hrithikkohli5678-cell <hrithikkohli5678@gmail.com>
2025-10-29 22:19:49 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -30,11 +30,12 @@ BEGIN
 		PRINT '------------------------------------------------';
 
 		SET @start_time = GETDATE();
+-- crm_cust_info
 PRINT '>> Truncating Table: bronze.crm_cust_info';
 TRUNCATE TABLE bronze.crm_cust_info;
 PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 BULK INSERT bronze.crm_cust_info
-FROM '/Users/hrithikkohli/Desktop/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+FROM 'C:\Users\hrithikkohli\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -46,7 +47,7 @@ PRINT '>> Truncating Table: bronze.crm_prd_info';
 TRUNCATE TABLE bronze.crm_prd_info;
 PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 BULK INSERT bronze.crm_prd_info
-FROM '/Users/hrithikkohli/Desktop/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
+FROM 'C:\Users\hrithikkohli\Desktop\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -58,19 +59,19 @@ PRINT '>> Truncating Table: bronze.crm_sales_details';
 TRUNCATE TABLE bronze.crm_sales_details;
 PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 BULK INSERT bronze.crm_sales_details
-FROM '/Users/hrithikkohli/Desktop/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
+FROM 'C:\Users\hrithikkohli\Desktop\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
     TABLOCK
 );
 
--- ERP tables follow same pattern
+-- ERP tables
 PRINT '>> Truncating Table: bronze.erp_loc_a101';
 TRUNCATE TABLE bronze.erp_loc_a101;
 PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 BULK INSERT bronze.erp_loc_a101
-FROM '/Users/hrithikkohli/Desktop/sql-data-warehouse-project/datasets/source_erp/LOC_A101.csv'
+FROM 'C:\Users\hrithikkohli\Desktop\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -81,7 +82,7 @@ PRINT '>> Truncating Table: bronze.erp_cust_az12';
 TRUNCATE TABLE bronze.erp_cust_az12;
 PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 BULK INSERT bronze.erp_cust_az12
-FROM '/Users/hrithikkohli/Desktop/sql-data-warehouse-project/datasets/source_erp/CUST_AZ12.csv'
+FROM 'C:\Users\hrithikkohli\Desktop\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -92,7 +93,7 @@ PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 BULK INSERT bronze.erp_px_cat_g1v2
-FROM '/Users/hrithikkohli/Desktop/sql-data-warehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
+FROM 'C:\Users\hrithikkohli\Desktop\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
